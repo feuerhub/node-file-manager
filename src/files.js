93fd28b4bc939a64b3c0a64040ea35a4daa806b3
Mikhail Markov <mikelmmv@gmail.com>
2024-10-14 23:29:40 +0500

    feat: add cat and add operations

--- src/files.js
+++ src/files.js
@@ -1,10 +1,18 @@
-import { readdir } from 'node:fs';
+import fs from 'fs';
 
-export const list = async (filePath) => {
-    readdir(filePath, (err, files) => {
-        if (err) {
-            throw new Error('FS operation failed');
-        }
-        console.log(files);
-    })
-};
+export const cat = async (filePath) => {
+  const readableStream = fs.createReadStream(filePath, { encoding: 'utf-8' });
+  readableStream.on('error', () => {
+    console.log('Operation failed');
+  });
+  readableStream.pipe(process.stdout);
+}
+
+export const add = async (filePath) => {
+  try {
+    await fs.promises.writeFile(filePath, '');
+  } catch {
+    console.log('Operation failed');
+  }
+}
+
